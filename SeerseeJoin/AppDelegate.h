4c260e1760c4edadf8669f64191acb683ee91345
Mr-Hawaii <396503820@qq.com>
2016-09-05 13:59:34 +0800

    Initial Commit

---
+++ SeerseeJoin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeerseeJoin
+//
+//  Created by 赵云锋 on 16/9/5.
+//  Copyright © 2016年 hawaii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SeerseeJoin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeerseeJoin
+//
+//  Created by 赵云锋 on 16/9/5.
+//  Copyright © 2016年 hawaii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
